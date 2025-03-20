6ca7c34fa7d8cf1ad7199dbb0c3e735fc55076c2
国栋 <wangzunjiao.wzj@digital-engine.com>
2025-03-20 18:09:38 +0800

    公用数据上传

---
+++ src/main/java/com/antom/AlipayClient.java
@@ -0,0 +1,11 @@
+package com.antom;
+
+import com.antom.exception.AlipayApiException;
+import com.antom.request.AlipayRequest;
+import com.antom.response.AlipayResponse;
+
+public interface AlipayClient {
+
+    <T extends AlipayResponse> T execute(AlipayRequest<T> alipayRequest) throws AlipayApiException;
+
+}
---
+++ src/main/java/com/antom/net/HttpMethod.java
@@ -0,0 +1,7 @@
+package com.antom.net;
+
+public enum HttpMethod {
+
+    POST,GET;
+
+}
