a85ad145179013b44211858062b061351286a5e9
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2025-03-27 08:23:47 +0000

    false[antom-sdk-automation] automated change

--- src/main/java/com/antom/request/AlipayRequest.java
+++ src/main/java/com/antom/request/AlipayRequest.java
@@ -1,41 +1,41 @@
 package com.antom.request;
 
 import com.alibaba.fastjson.annotation.JSONField;
-
 import com.antom.response.AlipayResponse;
-import lombok.Data;
 import javax.ws.rs.HttpMethod;
+import lombok.Data;
 
 @Data
 public abstract class AlipayRequest<T extends AlipayResponse> {
-    /**
-     * client id
-     */
-    @JSONField(serialize = false)
-    private String   clientId;
-    @JSONField(serialize = false)
-    private String   path;
-    @JSONField(serialize = false)
-    private Integer  keyVersion;
-    @JSONField(serialize = false)
-    private Class<T> responseClass;
-    @JSONField(serialize = false)
-    private String   httpMethod = HttpMethod.POST;
+  /** client id */
+  @JSONField(serialize = false)
+  private String clientId;
 
-    /**
-     * 是否使用沙箱url
-     *
-     * @return true/false
-     */
-    public boolean usingSandboxUrl() {
-        return true;
-    }
+  @JSONField(serialize = false)
+  private String path;
 
-    /**
-     * 得到当前API的响应结果类型
-     *
-     * @return 响应类型
-     */
-    public abstract Class<T> getResponseClass();
+  @JSONField(serialize = false)
+  private Integer keyVersion;
 
+  @JSONField(serialize = false)
+  private Class<T> responseClass;
+
+  @JSONField(serialize = false)
+  private String httpMethod = HttpMethod.POST;
+
+  /**
+   * 是否使用沙箱url
+   *
+   * @return true/false
+   */
+  public boolean usingSandboxUrl() {
+    return true;
+  }
+
+  /**
+   * 得到当前API的响应结果类型
+   *
+   * @return 响应类型
+   */
+  public abstract Class<T> getResponseClass();
 }
