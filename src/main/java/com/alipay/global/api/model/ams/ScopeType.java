769b02b22d854e3dd3966f4b83d8eee7576831e9
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2025-04-07 10:33:09 +0000

    false[antom-sdk-automation] automated change

--- src/main/java/com/alipay/global/api/model/ams/ScopeType.java
+++ src/main/java/com/alipay/global/api/model/ams/ScopeType.java
@@ -1,5 +1,11 @@
 package com.alipay.global.api.model.ams;
 
 public enum ScopeType {
-    BASE_USER_INFO, AGREEMENT_PAY, USER_INFO, USER_LOGIN_ID, HASH_LOGIN_ID, SEND_OTP, TAOBAO_REBIND;
+  BASE_USER_INFO,
+  AGREEMENT_PAY,
+  USER_INFO,
+  USER_LOGIN_ID,
+  HASH_LOGIN_ID,
+  SEND_OTP,
+  TAOBAO_REBIND;
 }
