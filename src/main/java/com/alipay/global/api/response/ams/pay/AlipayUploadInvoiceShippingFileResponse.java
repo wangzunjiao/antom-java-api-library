610d8fa713d6be5090a84a1755ee63cb63f50bbe
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2025-06-06 02:55:48 +0000

    false[antom-sdk-automation] automated change

--- src/main/java/com/alipay/global/api/response/ams/pay/AlipayUploadInvoiceShippingFileResponse.java
+++
@@ -1,6 +0,0 @@
-package com.alipay.global.api.response.ams.pay;
-
-import com.alipay.global.api.response.AlipayResponse;
-
-public class AlipayUploadInvoiceShippingFileResponse  extends AlipayResponse {
-}
